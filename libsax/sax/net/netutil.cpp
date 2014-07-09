1b9d94686138bed79e4754a60c32bf6ef3bbea26
zjx20 <zhoujianxiong2@gmail.com>
2014-07-09 19:49:50 +0800

    fixed rarely crash in netutil

--- libsax/sax/net/netutil.cpp
+++ libsax/sax/net/netutil.cpp
@@ -260,6 +260,8 @@ void transport::eda_callback(g_eda_t* mgr, int fd, void* user_data, int mask)
 			handle_tcp_accept(trans, fd);
 		}
 		else if (ctx.type == context::TCP_CONNECTION) {
+			// the connection may already be closed by handle_tcp_write()
+			if (UNLIKELY(_ctx[fd].tid.seq == -1)) return;
 			handle_tcp_read(trans, fd, ctx);
 		}
 		else if (ctx.type == context::UDP_BIND) {
